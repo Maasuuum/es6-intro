27eb74e97b743283a5cbce1a961e00ff92c1f7be
Maasuuum <fardousalmasum09@gmail.com>
2024-06-26 13:04:26 +0600

    template by backtick quote

---
+++ template.js
@@ -0,0 +1,11 @@
+const first = 'Jaan'
+const last = 'Paakhi'
+const greet = 'Potas Potas';
+const name = first + ' ' + last + " " + greet;
+console.log(name);
+const a = 20;
+const b = 10;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(result);
+const math = `The sum of ${a} and ${b} is ${a+b}`
+console.log(math);
