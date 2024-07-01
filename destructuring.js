d707ac0bba7b8dd08d82fbf5f4c384b05a82e485
Maasuuum <fardousalmasum09@gmail.com>
2024-07-01 06:56:03 +0600

    destucturing

---
+++ destructuring.js
@@ -0,0 +1,26 @@
+const actor = {
+    name: 'Ananata',
+    age: 30,
+    phone: '01717653844',
+    money: 1234567890101
+}
+
+const phone = actor.phone;
+const name = actor.name;
+const age = actor.age;
+
+console.log(phone)
+console.log(phone)
+console.log(phone)
+console.log(phone)
+console.log(phone)
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
+console.log(name)
+
+// array destructuring
+const numbers = [45, 99]
+const [first, second] = numbers;
+const [x, y] = [12, 66];
