5adbb3b7232574eb2d4acb9e048c18e35fbb8414
Maasuuum <fardousalmasum09@gmail.com>
2024-07-01 09:00:48 +0600

    looping

---
+++ looping.js
@@ -0,0 +1,12 @@
+const numbers = [1, 6, 8, 4];
+// for(let i=0; i<number.length; i++){}
+// while
+
+for(const num of numbers){
+    console.log(num);
+}
+
+const nobab = 'Siraj Ud Doula';
+for(const char of nobab){
+    console.log(char);
+}
