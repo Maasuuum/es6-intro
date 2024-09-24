50e48ac9367c765bac62d1c759279a4e37c7e75b
Maasuuum <fardousalmasum09@gmail.com>
2024-09-24 09:02:08 +0600

    revised template string

--- defaultnew.js
+++ defaultnew.js
@@ -1,3 +1,4 @@
+// default --> if value is not provided, take this as a default. 
 function add(num1, num2){
     const result = num1 + num2;
     console.log(num1, num2, result);
---
+++ templatenew.js
@@ -0,0 +1,26 @@
+const first = 'Fardous'
+const middle = 'Al'
+const last = 'Masum'
+
+const name = first + ' ' + middle + ' ' + last;
+
+console.log(name);
+
+const a = 10;
+const b = 20;
+const result = 'The sum of ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+const math = `The sum of ${a} and ${b} is ${a+b}`
+console.log(math);
+
+// Multiple line string
+const email = `Hello there! \n` +
+`How are you? \n` +
+`How old are you?`
+;
+console.log(email);
+
+const email2 = `Hello there!
+How are you?
+How old are you?`;
+console.log(email2);
