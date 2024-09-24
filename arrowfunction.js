5e3454eb29ec6ee2ee4937e56aaa8b777332197f
Maasuuum <fardousalmasum09@gmail.com>
2024-09-24 09:29:04 +0600

    arro function revised

---
+++ arrowfunction.js
@@ -0,0 +1,20 @@
+// function add (a, b){
+//     const result = a + b;
+//     return result;
+// }
+function add (a, b){
+    return a + b;
+}
+
+// function expression
+const add2 = function(a, b){
+    return a + b;
+}
+
+// arrow function
+const add3 = (a, b) => a + b
+const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 + num4;
+
+// const sum = add3(5, 90);
+const sum = add4(4, 5, 6, 7);
+console.log(sum);
