73ad21d197cf1c3977610497af706d57c6d8a0f0
Maasuuum <fardousalmasum09@gmail.com>
2024-09-26 14:10:08 +0600

    keys values entries

---
+++ keyysss.js
@@ -0,0 +1,15 @@
+const cigerattee = { 
+    name: 'benson', 
+    color: 'golden', 
+    price: 12, 
+    isCleaned: true
+};
+
+console.log(cigerattee);
+const keys = Object.keys(cigerattee);
+
+const values = Object.values(cigerattee);
+// console.log(values);
+
+const pair = Object.entries(cigerattee);
+console.log(pair);
