e2a57b77d064385a38fbd803464472d30200f508
Maasuuum <fardousalmasum09@gmail.com>
2024-09-26 12:34:14 +0600

    spread operator basic

---
+++ more-arrowfunction.js
@@ -0,0 +1,12 @@
+// const difference = (x, y) => x - y;
+// const multiply = (first, second, third) => first * second * third;
+
+// const getAge = (person) => person.age;
+// const student = { name: 'ananata', age: 45}
+// const age = getAge(student)
+// console.log(age);
+
+const getThird = numbers => numbers[2];
+const third = getThird([5,9,88,2,13])
+console.log(third)
+
